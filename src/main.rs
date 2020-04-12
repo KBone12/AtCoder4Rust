dda6685f112f362aa6d041c2a0037bacf3c29475
kbone <kbonehobby@gmail.com>
2020-04-13 02:07:09 +0900

    Request an argument, 'contest id'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use clap::{app_from_crate, crate_authors, crate_description, crate_name, crate_version, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let args = app_from_crate!()
+        .arg(Arg::with_name("contest id").index(1).required(true))
+        .get_matches();
+    let contest_id = args.value_of("contest id").unwrap();
+    println!("contest id: {}", contest_id);
 }
